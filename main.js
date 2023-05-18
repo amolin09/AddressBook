12b9a3652d8fcf0087f6ee9ba6444fc6fc6223fc
amolin09 <alex.molina5829@gmail.com>
2023-05-17 21:15:39 -0500

    finished logic and DOM

--- main.js
+++ main.js
@@ -1,22 +1,83 @@
-async function logRandomUser() {
-  const response = await fetch("https://randomuser.me/api/");
-  const jsonData = await response.json();
-  console.log(jsonData);
+async function getUserInfo() {
+  const res = await fetch("https://randomuser.me/api/")
+
+  const jsonData = await res.json()
+
+  return jsonData
 }
 
+
+
+async function getMultipleUserInfo(amountOfUsers) {
+  const res = await fetch(`https://randomuser.me/api/?results=${amountOfUsers}`)
+
+  const jsonData = await res.json();
+
+ return jsonData
+}
+
+
 let addressEntries = []
-let addressEntriesNames = []
+let hasContent = false
 
-let newEntry = logRandomUser()
-let addressEntryName = newEntry.name
-addressEntriesNames.push(addressEntryName)
 
-// for(let i = 0; i < 1; i++){
+async function addDataToArray() {
+
+  const userData = await getMultipleUserInfo(5)
+ addressEntries = userData.results
+}
+
+
+const displayUser = (person, i) => {
   
-//   let newEntry = logRandomUser()
-//   let addressEntryName = newEntry.name
-  
-//   addressEntriesNames.push(addressEntryName)
-// }
+  const li = document.createElement('li');
+  li.id = `person-${i}`;
 
-console.log(addressEntriesNames)
+  
+  const userPicture = person.picture.thumbnail;
+  const imgEl = document.createElement('img')
+  imgEl.src = userPicture;
+  li.appendChild(imgEl);
+
+  const userName = person.name.first;
+  const userNameText = document.createTextNode(userName);
+  li.append(userNameText);
+
+  const buttonEl = document.createElement('button');
+  buttonEl.innerText = "Show Info";
+  buttonEl.setAttribute("onclick",`showInfo('${li.id}', '${JSON.stringify(person)}')`);
+  li.append(buttonEl);
+
+  const addressListEl = document.getElementById('address-list');
+  addressListEl.appendChild(li);
+}
+
+
+const showInfo = (id, person) => {
+  const li = document.getElementById(id);
+
+  if(hasContent){
+    li.removeChild(li.lastChild)
+    hasContent = false
+  }
+  else{
+    const personInfo = document.createTextNode(person);
+    li.appendChild(personInfo);
+    hasContent = true
+  }
+ 
+  
+}
+
+
+const displayAddressList = async () => {
+   await addDataToArray();
+
+    let i = 0;
+  for(person of addressEntries) {
+    displayUser(person, i++);
+  }
+}
+
+displayAddressList();
+
